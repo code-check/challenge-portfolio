b0e4de7fcceb2f1cbbe25ffffd50d8097a97ad22
takayukioda <takayuki@odashi.me>
2016-01-18 14:09:49 +0900

    Fix database syntax

--- specifications/database.sql
+++ specifications/database.sql
@@ -1,7 +1,7 @@
-create projects (
+create table projects (
     id integer PRIMARY KEY AUTOINCREMENT,
     url varchar(255) NULL,
     title varchar(255) NOT NULL,
-    description text NOT NULL
+    description text NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   );
