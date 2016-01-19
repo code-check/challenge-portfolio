e208c080c82ba99a5c941c7d716b2b33a7f733cc
takayukioda <takayuki@odashi.me>
2016-01-19 09:34:26 +0900

    Remove duplicated file

--- database.sql
+++
@@ -1,7 +0,0 @@
-create projects (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    url varchar(255) NULL,
-    title varchar(255) NOT NULL,
-    description text NOT NULL
-    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
